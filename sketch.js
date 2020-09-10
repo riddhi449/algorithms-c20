bf5b2c0f775bcc648a501f4c9728eb1a5b35b637
riddhi449 <70949645+riddhi449@users.noreply.github.com>
2020-09-10 18:05:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+var fixedrect,movingrect;
+
+
+
+
+function setup() {
+  createCanvas(800,400);
+  fixedrect = createSprite(400, 200, 50, 50);
+  movingrect = createSprite(400,200,100,50);
+  fixedrect.shapeColor = "orange";
+  movingrect.shapeColor = "orange";
+}
+
+function draw() {
+  background(255,255,255);
+  movingrect.x = mouseX;
+  movingrect.y = mouseY;
+
+  if(movingrect.x - fixedrect.x <= (movingrect.width/2 +fixedrect.width/2) && 
+  fixedrect.x - movingrect.x <=(movingrect.width/2 +fixedrect.width/2) 
+  && movingrect.y - fixedrect.y <= (movingrect.height/2 + fixedrect.height/2) && 
+    fixedrect.y - movingrect.y <= (movingrect.height/2 + fixedrect.height/2)) {
+    fixedrect.shapeColor ="blue";
+    movingrect.shapeColor = "blue";
+  }
+
+  else{
+    fixedrect.shapeColor = "orange";
+    movingrect.shapeColor = "orange";
+  }
+
+  
+  
+  drawSprites();
+}
+
